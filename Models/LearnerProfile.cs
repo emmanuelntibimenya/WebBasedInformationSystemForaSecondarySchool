3ddf5f21a2edf692ca85777e724292eeb7d5c6fc
emmanuel <emmanuelntibimenya@gmail.com>
2022-06-19 13:41:01 +0100

    Added a new entity: LearnerProfile

---
+++ Models/LearnerProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace SchoolManagementSystem.Models
+{
+    public class LearnerProfile
+    {
+        public  int ID { get; set; }
+        public string Name { get; set; }
+        public string Note { get; set; }
+        public LearnerProfile()
+        {
+            Note = string.Empty;
+
+        }
+    }
+}
